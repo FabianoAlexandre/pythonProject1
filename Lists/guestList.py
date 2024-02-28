39a7ab74bd4f10a3496ce4d2fda1357d2d121483
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2024-02-28 23:24:56 +0000

    Trabalhando com listas

---
+++ Lists/guestList.py
@@ -0,0 +1,16 @@
+dinnerInvitation = ["anna","suely","marile"]
+print("Hello " + dinnerInvitation[0] + ", would you like dinner with me tonight")
+print("hey " + dinnerInvitation[1] + " let's dinner with me")
+print("Hi " + dinnerInvitation[2] + " do you want to dinner with me dear")
+print("Sorry, but sra. Anna can't go to the dinner")
+dinnerInvitation[0] = "Laura"
+print(dinnerInvitation)
+print("Hello " + dinnerInvitation[0].title() + ", would you like dinner with me tonight")
+print("hey " + dinnerInvitation[1].upper() + " let's dinner with me")
+print("Hi " + dinnerInvitation[2].lower() + " do you want to dinner with me dear")
+print ("Hey folks, I found a bigger table.")
+dinnerInvitation.append("Ruan")
+dinnerInvitation.insert(2,"Elaine")
+dinnerInvitation.insert(5,"daniele")
+print("\n\t WELCOME \n" +dinnerInvitation[0].title()+ " This is my daughter \n" + dinnerInvitation[1] + " This is my lovely mom-S2 \n" + dinnerInvitation[2].upper() + " This is my yunger sister \n" +dinnerInvitation[3].lower()+ " This is my aunt \n" +dinnerInvitation[4].upper()+ " This is my first son \n" +dinnerInvitation[5].title()+ " This is my lovely friend :-)"
+      )
---
+++ Lists/updateDinnerImvitation.py
@@ -0,0 +1,12 @@
+dinnerInvitation = ["anna","suely","marile"]
+print(dinnerInvitation)
+print("Hello " + dinnerInvitation[0] + ", would you like dinner with me tonight")
+print("hey " + dinnerInvitation[1] + " let's dinner with me")
+print("Hi " + dinnerInvitation[2] + " do you want to dinner with me dear")
+print("Sorry, but sra. Anna can't go to the dinner")
+dinnerInvitation[0] = "Laura"
+print("\tUpdate List")
+print(dinnerInvitation)
+print("Hello " + dinnerInvitation[0].title() + ", would you like dinner with me tonight")
+print("hey " + dinnerInvitation[1].upper() + " let's dinner with me")
+print("Hi " + dinnerInvitation[2].lower() + " do you want to dinner with me dear")
